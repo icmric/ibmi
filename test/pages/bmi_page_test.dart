6feee45b676e392d17ebdac423bff7fec2023649
icmeric <70637822+icmeric@users.noreply.github.com>
2023-02-28 17:20:25 +1100

    added widget tests

--- test/pages/bmi_page_test.dart
+++ test/pages/bmi_page_test.dart
@@ -7,7 +7,7 @@ void main() {
       'Given WeightInfoCard When user clicks + button Then weight increment by 1',
       (tester) async {
     //Arrange
-    tester.pumpWidget(
+    await tester.pumpWidget(
       CupertinoApp(
         home: BMIPage(),
       ),
@@ -17,5 +17,24 @@ void main() {
     await tester.tap(_weightIncrementButton);
     await tester.pump();
     //Assert
+    var _text = find.text("161");
+    expect(_text, findsOneWidget);
+  });
+  testWidgets(
+      'Given WeightInfoCard When user clicks - button Then weight decrement by 1',
+      (tester) async {
+    //Arrange
+    await tester.pumpWidget(
+      CupertinoApp(
+        home: BMIPage(),
+      ),
+    );
+    var _weightIncrementButton = find.byKey(Key('weight_minus'));
+    //Act
+    await tester.tap(_weightIncrementButton);
+    await tester.pump();
+    //Assert
+    var _text = find.text("159");
+    expect(_text, findsOneWidget);
   });
 }
